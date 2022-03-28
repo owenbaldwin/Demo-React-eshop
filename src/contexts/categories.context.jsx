dadc4168af3498958c02a32225638c17a5547f2a
Owen Baldwin <owenabaldwin@gmail.com>
2022-03-28 17:16:36 +0100

    first half of the firebase data storage done

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,31 @@
+import { createContext, useState, useEffect } from "react";
+import { addCollectionAndDocuments, getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
+import SHOP_DATA from '../shop-data.js';
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+});
+
+export const CategoriesProvider = ({children}) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+
+  // One-off function to add a JS object db to the firebase db to not have to write it by hand in firebase:
+  // useEffect(() => {
+  //   addCollectionAndDocuments('categories', SHOP_DATA);
+  // }, [])
+
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+      setCategoriesMap(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
+  const value = { categoriesMap };
+  return (
+    <CategoriesContext.Provider value={value}> {children} </CategoriesContext.Provider>
+  );
+};
