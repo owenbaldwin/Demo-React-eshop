513d65fbe63574f76b525750e15aa1d8e3c0add4
Owen Baldwin <owenabaldwin@gmail.com>
2022-03-30 18:04:22 +0100

    all styling redone except form inputs

--- src/components/cart-item/cart-item.styles.jsx
+++ src/components/cart-item/cart-item.styles.jsx
@@ -9,6 +9,11 @@ export const CartItemContainer = styled.div`
 
   img {
     width: 30%;
+    background:
+    linear-gradient(#29263A, #29263A) padding-box,
+    linear-gradient(135deg, #DDBC9C, #E19B6A) border-box;
+    border-radius: 10px;
+    border: 1px solid transparent;
   }
 `;
 
@@ -23,5 +28,22 @@ export const ItemDetails = styled.div`
 
   .name {
     font-size: 16px;
+    background-image: linear-gradient(
+      135deg,
+      #DDBC9C 0%,
+      #E19B6A 50%
+    );
+    color: transparent;
+    -webkit-background-clip:text;
+  }
+  .price {
+    font-size: 16px;
+    background-image: linear-gradient(
+      135deg,
+      #DDBC9C 0%,
+      #E19B6A 50%
+    );
+    color: transparent;
+    -webkit-background-clip:text;
   }
 `;
--- src/routes/category/category.styles.jsx
+++ src/routes/category/category.styles.jsx
@@ -12,4 +12,11 @@ export const CategoryTitle = styled.h2`
   font-size: 38px;
   margin-bottom: 25px;
   text-align: center;
+  background-image: linear-gradient(
+    135deg,
+    #DDBC9C 0%,
+    #E19B6A 50%
+  );
+  color: transparent;
+  -webkit-background-clip:text;
 `;
